b6849dbf4233a5f7a45a9d6605aa0f9effb78eaa
k.gvozdovsky <kirillgvozd0@gmail.com>
2025-11-10 16:48:18 +0300

    Made gRPC server on Java

---
+++ gRPC_server/build.gradle.kts
@@ -0,0 +1,72 @@
+import com.google.protobuf.gradle.*
+
+plugins {
+    id("java")
+    id("com.google.protobuf") version "0.9.4"
+    id("idea")
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+val grpcVersion = "1.76.0"
+
+repositories {
+    mavenCentral()
+}
+dependencies {
+    implementation("io.grpc:grpc-protobuf:${grpcVersion}")
+    implementation("io.grpc:grpc-stub:${grpcVersion}")
+    implementation("io.grpc:grpc-netty:${grpcVersion}")
+    implementation("com.google.protobuf:protobuf-java:4.33.0")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+
+    compileOnly("io.grpc:grpc-netty:")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:4.33.0"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+        }
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                id("grpc")
+            }
+        }
+    }
+}
+
+sourceSets {
+    named("main") {
+        java {
+            srcDir("build/generated/source/proto/main/grpc")
+            srcDir("build/generated/source/proto/main/java")
+        }
+    }
+}
+
+//tasks.jar {
+//    manifest {
+//        attributes["Main-Class"] = "ecommerce.ProductInfoServer"
+//    }
+//    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+//}
+//
+//application {
+//    mainClass.set("ecommerce.ProductInfoServer")
+//}
+
+tasks.startScripts {
+    enabled = false
+}
